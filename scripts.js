008cb08ebcae0707ca674238f56386b9169754eb
Muhammad Sojib <127195773+Muhammads-github@users.noreply.github.com>
2025-01-27 13:35:37 +0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// If you want to add interactivity, like smooth scroll, animations, or more actions,
-// you can add JS here.
-
-document.querySelector(".button").addEventListener("click", function () {
-  alert("Booking page is coming soon!");
-});
