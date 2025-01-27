509091b7d8f19843a8e347c017be58d7dcd536d0
Muhammad Sojib <127195773+Muhammads-github@users.noreply.github.com>
2025-01-27 13:37:06 +0600

    made a tour website

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// If you want to add interactivity, like smooth scroll, animations, or more actions,
+// you can add JS here.
+
+document.querySelector(".button").addEventListener("click", function () {
+  alert("Booking page is coming soon!");
+});
